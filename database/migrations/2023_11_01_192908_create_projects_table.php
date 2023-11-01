1f90a375af60d2b0acfd68ed5b8c0c0313b7eb26
Bilal-kassab <bilok8218@gmail.com>
2023-11-01 23:06:53 +0200

    first commit

---
+++ database/migrations/2023_11_01_192908_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100)->nullable();
+            $table->string('introduction',500)->nullable();
+            $table->string('location',255)->nullable();
+            $table->decimal('cost',22)->nullable()->default(0.00);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
